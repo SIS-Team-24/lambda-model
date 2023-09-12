6c011bdb07780679b14e8d6e80aa647f32059e12
Henry <13032204@student.uts.edu.au>
2023-09-12 20:33:47 +1000

    Add action to deploy dependencies as layer to AWS S3

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,30 +1,19 @@
 import os
 import json
-from happytransformer import HappyTextToText, TTSettings
-
-# Manually call save_model to overwrite the `model` folder before uploading to AWS
-def save_model():
-    model = HappyTextToText(model_type="BART", model_name="facebook/bart-large-cnn")
-    model.save("model/")
+from transformers import pipeline
 
 def lambda_handler(event, context):
-    # Load model during function initialization
-    model = HappyTextToText(model_type="BART", model_name="model/")
-
+    # using pipeline API for summarization task
+    summarization = pipeline("summarization")
     # Get the input text from Lambda event
     input_text = event['input_text']
 
-    # Summarization settings
-    summary_args = TTSettings(min_length=1, max_length=500)
-
-    # Generate the summary
-    result= model.generate_text(input_text, summary_args)
+    summary_text = summarization(input_text)[0]['summary_text']
 
     return {
         "statusCode" : 200,
-        "summary" : result.text
+        "summary" : summary_text
     }
 
 if __name__ == '__main__':
-    save_model()
-    print(lambda_handler())
+    print(lambda_handler(event={'input_text' : "Hello world, this is a test summarization tool."}, context=''))
